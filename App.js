386dafc10be69d6d36560383fb22d14ddf1e677d
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-09-01 23:41:57 +0700

    Update App.js
    add animation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, TransitionPresets } from "@react-navigation/stack";
 import StartPage from "./app/screens/StartPage";
 import LoginPage from "./app/screens/LoginPage";
 import HomePage from "./app/screens/HomePage";
@@ -12,10 +12,17 @@ const Stack = createStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Start">
-                <Stack.Screen name="Start" component={StartPage} options={{ headerShown: false }} />
-                <Stack.Screen name="Login" component={LoginPage} options={{ headerShown: false }} />
-                <Stack.Screen name="Register" component={RegisterPage} options={{ headerShown: false }} />
+            <Stack.Navigator
+                initialRouteName="Start"
+                screenOptions={{
+                    headerShown: false,
+                    gestureEnabled: true,
+                    ...TransitionPresets.SlideFromRightIOS,
+                }}
+            >
+                <Stack.Screen name="Start" component={StartPage} />
+                <Stack.Screen name="Login" component={LoginPage} />
+                <Stack.Screen name="Register" component={RegisterPage} />
                 <Stack.Screen name="MoreInfo" component={MoreInfoPage} />
                 <Stack.Screen name="Home" component={HomePage} />
             </Stack.Navigator>
